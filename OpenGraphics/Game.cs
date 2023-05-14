f040d7703c039c1abbedbd0759bb2edeebf414aa
Martivis <vanek-non@rambler.ru>
2023-05-14 15:17:36 +0300

    Add camera

--- OpenGraphics/Game.cs
+++ OpenGraphics/Game.cs
@@ -2,6 +2,7 @@
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 using System.Diagnostics;
 
 namespace OpenGraphics;
@@ -15,10 +16,16 @@ public class Game : GameWindow
     Shader _shader;
     Texture _texture;
 
-    private Matrix4 _view;
-    private Matrix4 _projection;
+    Camera _camera;
 
+    private float _aspectRatio;
     Stopwatch _stopwatch;
+
+    bool _firstMove = true;
+    Vector2 _lastCursorPos;
+
+    const float cameraSpeed = 1.5f;
+    const float sensitivity = 0.2f;
 
     private readonly float[] _vertices =
     {
@@ -27,10 +34,10 @@ public class Game : GameWindow
          0.0f,  0.5f, 0.0f, 1.0f, 0.0f,
          0.0f, -0.5f, 0.0f, 0.0f, 1.0f,
         
-        -0.5f, -0.0f, -0.2f, 0.0f, 0.0f,
-         0.5f,  0.0f, -0.2f, 1.0f, 1.0f,
-         0.0f,  0.5f, -0.2f, 0.0f, 1.0f,
-         0.0f, -0.5f, -0.2f, 1.0f, 0.0f,
+        -0.5f, -0.0f, -0.2f, 1.0f, 0.0f,
+         0.5f,  0.0f, -0.2f, 0.0f, 1.0f,
+         0.0f,  0.5f, -0.2f, 1.0f, 1.0f,
+         0.0f, -0.5f, -0.2f, 0.0f, 0.0f,
     };
 
     private readonly uint[] _indices =
@@ -58,30 +65,9 @@ public class Game : GameWindow
         : base(GameWindowSettings.Default, 
             new NativeWindowSettings() { Size = (width, height), Title = title }) 
     {
+        _aspectRatio = width / height;
     }
 
-    protected override void OnKeyDown(KeyboardKeyEventArgs e)
-    {
-        base.OnKeyDown(e);
-        switch (e.Key)
-        {
-            case OpenTK.Windowing.GraphicsLibraryFramework.Keys.A:
-                break;
-            case OpenTK.Windowing.GraphicsLibraryFramework.Keys.D:
-                break;
-            case OpenTK.Windowing.GraphicsLibraryFramework.Keys.S:
-                break;
-            case OpenTK.Windowing.GraphicsLibraryFramework.Keys.W:
-                break;
-            case OpenTK.Windowing.GraphicsLibraryFramework.Keys.Escape:
-                Close();
-                break;
-            case OpenTK.Windowing.GraphicsLibraryFramework.Keys.Enter:
-                break;
-            default:
-                break;
-        }
-    }
 
     protected override void OnLoad()
     {
@@ -101,8 +87,7 @@ public class Game : GameWindow
 
         GL.Enable(EnableCap.DepthTest);
 
-        _view = Matrix4.CreateTranslation(0.0f, 0.0f, -0.8f);
-        _projection = Matrix4.CreatePerspectiveFieldOfView(fovy: MathHelper.DegreesToRadians(90f), Size.X / (float)Size.Y, 0.1f, 100f);
+        _camera = new Camera(Vector3.UnitZ * 0.8f, _aspectRatio);
 
         _stopwatch = new Stopwatch();
         _stopwatch.Start();
@@ -153,6 +138,92 @@ public class Game : GameWindow
         GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
     }
 
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+        base.OnUpdateFrame(e);
+
+        if (!IsFocused)
+        {
+            return;
+        }
+
+        var input = KeyboardState;
+
+        if (input.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+
+        
+
+        if (input.IsKeyDown(Keys.W))
+        {
+            _camera.Position += _camera.Front * cameraSpeed * (float)e.Time; // Forward
+        }
+
+        if (input.IsKeyDown(Keys.S))
+        {
+            _camera.Position -= _camera.Front * cameraSpeed * (float)e.Time; // Backwards
+        }
+        if (input.IsKeyDown(Keys.A))
+        {
+            _camera.Position -= _camera.Right * cameraSpeed * (float)e.Time; // Left
+        }
+        if (input.IsKeyDown(Keys.D))
+        {
+            _camera.Position += _camera.Right * cameraSpeed * (float)e.Time; // Right
+        }
+        if (input.IsKeyDown(Keys.Space))
+        {
+            _camera.Position += _camera.Up * cameraSpeed * (float)e.Time; // Up
+        }
+        if (input.IsKeyDown(Keys.LeftShift))
+        {
+            _camera.Position -= _camera.Up * cameraSpeed * (float)e.Time; // Down
+        }
+
+        if (MouseState.IsButtonDown(MouseButton.Left))
+        {
+            CursorState = CursorState.Grabbed;
+            RotateCamara();
+        }
+        else
+        {
+            _firstMove = true;
+            CursorState = CursorState.Normal;
+        }
+    }
+
+    private void RotateCamara()
+    {
+        var mouse = MouseState;
+        if (_firstMove)
+        {
+            _lastCursorPos = new Vector2(mouse.X, mouse.Y);
+            _firstMove = false;
+        }
+        else
+        {
+            // Calculate the offset of the mouse position
+            var deltaX = mouse.X - _lastCursorPos.X;
+            var deltaY = mouse.Y - _lastCursorPos.Y;
+            _lastCursorPos = new Vector2(mouse.X, mouse.Y);
+
+            // Apply the camera pitch and yaw (we clamp the pitch in the camera class)
+            _camera.Yaw += deltaX * sensitivity;
+            _camera.Pitch -= deltaY * sensitivity; // Reversed since y-coordinates range from bottom to top
+        }
+    }
+
+    // In the mouse wheel function, we manage all the zooming of the camera.
+    // This is simply done by changing the FOV of the camera.
+    protected override void OnMouseWheel(MouseWheelEventArgs e)
+    {
+        base.OnMouseWheel(e);
+
+        _camera.Fov -= e.OffsetY;
+    }
+
     protected override void OnRenderFrame(FrameEventArgs e)
     {
         base.OnRenderFrame(e);
@@ -177,8 +248,8 @@ public class Game : GameWindow
 
 
         _shader.SetMatrix4("transform", transform);
-        _shader.SetMatrix4("view", _view);
-        _shader.SetMatrix4("projection", _projection);
+        _shader.SetMatrix4("view", _camera.GetViewMatrix());
+        _shader.SetMatrix4("projection", _camera.GetProjectionMatrix());
     }
 
     private void DrawObject()
@@ -199,8 +270,9 @@ public class Game : GameWindow
     protected override void OnResize(ResizeEventArgs e)
     {
         base.OnResize(e);
-
         GL.Viewport(0, 0, e.Width, e.Height);
+        _aspectRatio = e.Width / e.Height;
+        _camera.AspectRatio = _aspectRatio;
     }
 
     protected override void OnUnload()
