3d333041c39d153e78aa27c052dcf564776b57f9
Martivis <vanek-non@rambler.ru>
2023-05-15 00:28:06 +0300

    Add fps counter

--- OpenGraphics/Game.cs
+++ OpenGraphics/Game.cs
@@ -16,6 +16,9 @@ public class Game : GameWindow
     private float _aspectRatio;
     Stopwatch _stopwatch;
 
+    private uint _framesCount;
+    private double _lastCutoff;
+
     bool _firstMove = true;
     Vector2 _lastCursorPos;
 
@@ -164,6 +167,8 @@ public class Game : GameWindow
             _firstMove = true;
             CursorState = CursorState.Normal;
         }
+
+        CountFps();
     }
 
     private void RotateCamara()
@@ -187,8 +192,18 @@ public class Game : GameWindow
         }
     }
 
-    // In the mouse wheel function, we manage all the zooming of the camera.
-    // This is simply done by changing the FOV of the camera.
+    private void CountFps()
+    {
+        _framesCount++;
+        if (_stopwatch.Elapsed.TotalSeconds >= _lastCutoff + 1)
+        {
+            _lastCutoff = _stopwatch.Elapsed.TotalSeconds;
+            Console.Clear();
+            Console.WriteLine($"Fps: {_framesCount}");
+            _framesCount = 0;
+        }
+    }
+
     protected override void OnMouseWheel(MouseWheelEventArgs e)
     {
         base.OnMouseWheel(e);
